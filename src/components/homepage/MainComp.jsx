cb788f7d6e8fa53a834556bee18cfd39bd5cee06
OnlineAzamat <yakubbaev.azamat@mail.ru>
2023-03-01 20:47:39 +0500

    update 01.03.2022

--- src/components/homepage/MainComp.jsx
+++ src/components/homepage/MainComp.jsx
@@ -1,60 +1,82 @@
-
+import logo from "../../assets/logo.png"
 
 export default function mainComp() {
     return (
         <main>
             <nav>
                 <div className="inner-nav">
-                    <div className="time-location">
-                        <div className="location">
-                            <div className="icon">
-                                <i className="bi bi-geo-alt"></i>
-                            </div>
-                            <span className="location-title">
-                                <span className="txt">
-                                    Ваш город: Москва
-                                </span>
-                                <span>
-                                    <i className="bi bi-chevron-down"></i>
-                                </span>
-                            </span>
+                    <div className="phone">
+                        <div className="icon">
+                            <i className="bi bi-telephone"></i>
                         </div>
-                        <div className="time">
-                            <div className="icon">
-                                <i className="bi bi-clock"></i>
+                        <span className="txt">
+                            +998888810188
+                        </span>
+                    </div>
+                    <div className="get50">
+                        <p>Get 50% Off on Selected Items</p>
+                        <span>|</span>
+                        <a href="/">
+                            Shop Now
+                        </a>
+                    </div>
+                    <div className="lang-location">
+                        <div className="lang">
+                            <span>Eng:</span>
+                            <i className="bi bi-chevron-down"></i>
+                            <div className="all-item">
+                                <span>Ру</span>
+                                <span>Qq</span>
                             </div>
-                            <span className="txt">
-                                Пн-Пт 9:00 - 19:00
-                            </span>
+                        </div>
+                        <div className="lang location">
+                            <span>Location:</span>
+                            <i className="bi bi-chevron-down"></i>
                         </div>
                     </div>
-                    <ul className="navbar">
-                        <li>
-                            <a href="/">
-                                О компании
-                            </a>
-                        </li>
-                        <li>
-                            <a href="/">
-                                Преимущества
-                            </a>
-                        </li>
-                        <li>
-                            <a href="/">
-                                Акционные товары
-                            </a>
-                        </li>
-                        <li>
-                            <a href="/">
-                                <i className="bi bi-person-circle"></i>
-                                <span>Войти в аккаунт</span>
-                            </a>
-                        </li>
-                    </ul>
                 </div>
             </nav>
             <header>
-                header
+                <div className="inner-head">
+                    <div className="logo">
+                        <img src={logo} alt="logo" />
+                        <h1>ShopCart</h1>
+                    </div>
+                    <ul>
+                        <li>
+                            <a href="/">
+                                <span>Categories</span>
+                                <i className="bi bi-chevron-down"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/">
+                                Deals
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/">
+                                What's New
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/">
+                                Delivery
+                            </a>
+                        </li>
+                    </ul>
+                    <div className="inner_search">
+                        <input type="search" name="a" id="a" />
+                    </div>
+                    <div className="account">
+                        <i className="bi bi-person"></i>
+                        <span>Account</span>
+                    </div>
+                    <div className="cart">
+                        <i className="bi bi-cart"></i>
+                        <span>Cart</span>
+                    </div>
+                </div>
             </header>
         </main>
     )
